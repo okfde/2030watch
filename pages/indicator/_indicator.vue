2bc90ffb85827bde29bdd79ffb3d5e77e94fda0c
Jonas <jonasparnow@gmail.com>
2018-04-19 18:55:05 +0200

    indicator new layout

--- pages/indicator/_indicator.vue
+++ pages/indicator/_indicator.vue
@@ -1,9 +1,28 @@
 <template>
-  <div class="container">
-    <nav><nuxt-link to="/">Übersicht</nuxt-link><span class="spacer">/</span><nuxt-link :to="'../sdg/' + indicator.slugSdg">{{ indicator.sdg }}</nuxt-link><span class="spacer">/</span>{{ indicator.topic }}</nav>
-    <h1>{{ indicator.topic }}</h1>
-    <h3><nuxt-link :to="'../sdg/' + indicator.slugSdg">← {{ indicator.sdg }}</nuxt-link></h3>
-    <span>{{ indicator.labelValue }}</span>
+  <div class="page-container">
+    <nav class="inpage-nav">
+      <div class="wrapper">
+        <nuxt-link to="/">Übersicht</nuxt-link><span class="spacer">/</span><nuxt-link :to="'../sdg/' + indicator.slugSdg">{{ indicator.sdg }}</nuxt-link><span class="spacer">/</span>{{ indicator.topic }}
+      </div>
+    </nav>
+    <header class="inpage-header">
+      <div class="wrapper">
+        <h1>{{ indicator.topic }}</h1>
+        <section class="columns columns-gutter">
+          <div>
+            <p>Die Gleichberechtigung der Geschlechter ist ein elementarer Faktor für nachhaltige Entwicklung weltweit. Frauen müssen gleichgestellt sich an allen Entscheidungen beteiligen können, die ihr Leben beeinflussen.</p>
+          </div>
+          <div>
+            <strong>{{ indicator.labelValue }}</strong>
+          </div>
+        </section>
+      </div>
+    </header>
+    <div class="content">
+      <div class="wrapper">
+        {{ indicator.sdg }}
+      </div>
+    </div>
   </div>
 </template>
 
