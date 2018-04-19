1c74dc3c224a9ccde530082285af7643bac3d758
Jonas <jonasparnow@gmail.com>
2018-04-19 16:15:39 +0200

    Intro Text update

--- components/Slideshow.vue
+++ components/Slideshow.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="slideshow">
-    <div>
-      <h2>Warum wir andere Indikatoren brauchen um Nachhaltigkeit zu messen:</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
-    <div>
-      <span>Grafik</span>
-    </div>
+    <section>
+      <p>Das Nachhaltigkeitsziel SDG 5 steht für die Gleichstellung der Geschlechter. Im Zuge der gesetzlichen Vorschriften sind bestimmte Unternehmen in Deutschland seit 2016 dazu verpflichtet, mindestens 30% Frauenanteil in den Aufsichtsräten zu erreichen. Auch die Bundesregierung dasselbe Ziel für SDG 5 gesetzt, jedoch bis 2030.</p>
+    </section>
+    <section>
+      <p>Die Vereinten Nationen sprechen von gleichberechtigter Partizipation von Frauen in Politik, Wirtschaft und öffentlichem Leben. Aus Sicht von 2030Watch ist daher das Ziel von 30% der DNS nicht ambitioniert genug. Mit der Betrachtung von lediglich 106 Unternehmen nimmt die DNS außerdem nur einen unzureichenden Bereich der Teilhabe in den Blick.</p>
+    </section>
+    <section>
+      <p>2030Watch fordert im Einklang mit SDG 5 einen Frauenanteil von 50% in Aufsichtsräten und anderen Führungsebenen. Ergänzend zur Wirtschaft ist 2030Watch das Thema der politischen Partizipation von Frauen ebenso wichtig. Daher ergänzt 2030Watch die DNS um die Betrachtung des Frauenanteils in Parlamenten. Auch beim Anteil von Frauen in Parlamenten wird ein starker Handlungsbedarf Deutschlands offensichtlich.</p>
+    </section>
+    <section>
+      <p>Viele weitere spannende Stories und Ergänzen finden sich in den weiteren SDGs. Lernt, diskutiert, entdeckt!</p>
+    </section>
   </div>
 </template>
 
