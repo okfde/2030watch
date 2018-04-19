805942c213fd55986c5dedd73ee1d03bbe95fccb
Jonas <jonasparnow@gmail.com>
2018-04-19 13:49:04 +0200

    better label placement

--- components/VisListe.vue
+++ components/VisListe.vue
@@ -7,18 +7,43 @@
       </nuxt-link>
       <svg class="sdg-vis">
         <line
+          class="range"
           x1="0%"
           y1="50%"
           x2="100%"
           y2="50%" />
+        <line
+          class="diff"
+          :x1="sdg.total + '%'"
+          y1="50%"
+          :x2="sdg.dns + '%'"
+          y2="50%" />
         <circle
-          :cx="sdg.okf + '%'"
+          class="sdg-marker sdg-marker-total"
+          :cx="sdg.total + '%'"
           cy="50%"
           r="5" />
+        <text
+          class="sdg-label sdg-label-total"
+          alignment-baseline="baseline"
+          :text-anchor="sdg.total > 50 ? 'end' : 'start'"
+          :x="sdg.total + (sdg.total > 50 ? -3 : 3) + '%'"
+          y="40%">
+          {{ sdg.total }}%
+        </text>
         <circle
+          class="sdg-marker sdg-marker-dns"
           :cx="sdg.dns + '%'"
           cy="50%"
           r="5" />
+        <text
+          class="sdg-label sdg-label-dns"
+          alignment-baseline="hanging"
+          :text-anchor="sdg.dns > 50 ? 'end' : 'start'"
+          :x="sdg.dns + (sdg.dns > 50 ? -3 : 3) + '%'"
+          y="60%">
+          {{ sdg.dns }}%
+        </text>
       </svg>
     </li>
   </ul>
@@ -82,9 +107,39 @@
     .sdg-vis {
       height: 100%;
 
-      line {
-        stroke: black;
+      .range {
+        stroke: #D0CFCE;
+        stroke-width: 1px;
+      }
+
+      .diff {
+        stroke: #80B0DC;
         stroke-width: 2px;
+      }
+
+      .sdg-marker {
+        border: #fff;
+        border-radius: 1px;
+
+        &.sdg-marker-total {
+          fill: #CE8394;
+        }
+
+        &.sdg-marker-dns {
+          fill: #749A4E;
+        }
+      }
+
+      .sdg-label {
+        font-size: 12px;
+
+        &.sdg-label-total {
+          fill: #CE8394;
+        }
+
+        &.sdg-marker-dns {
+          fill: #749A4E;
+        }
       }
     }
   }
