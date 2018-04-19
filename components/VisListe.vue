c6a105ed58929ffe419efa3bd80bcb3bfbf33e69
Jonas <jonasparnow@gmail.com>
2018-04-19 13:17:58 +0200

    list layout

--- components/VisListe.vue
+++ components/VisListe.vue
@@ -1,8 +1,25 @@
 <template>
   <ul>
-    <li v-for="(sdg, slug) in sdgs">
-      <nuxt-link :to="'sdg/' + slug">{{ sdg.label }}</nuxt-link>
-      {{ sdg.okf }}% {{ sdg.dns }}% {{ sdg.total }}%
+    <li v-for="(sdg, slug) in sdgs" class="sdg-item">
+      <nuxt-link :to="'sdg/' + slug" class="sdg-label">
+        <img :src="'http://via.placeholder.com/50/' + sdg.color + '/' + sdg.color">
+        {{ sdg.label }}
+      </nuxt-link>
+      <svg class="sdg-vis">
+        <line
+          x1="0%"
+          y1="50%"
+          x2="100%"
+          y2="50%" />
+        <circle
+          :cx="sdg.okf + '%'"
+          cy="50%"
+          r="5" />
+        <circle
+          :cx="sdg.dns + '%'"
+          cy="50%"
+          r="5" />
+      </svg>
     </li>
   </ul>
 </template>
@@ -48,6 +65,28 @@
 </script>
 
 <style lang="scss">
+  .sdg-item {
+    display: flex;
+    height: 30px;
+    margin: 1em 0;
 
+    .sdg-label {
+      flex: 2;
+
+      img {
+        height: 50%;
+        border-radius: 50%;
+      }
+    }
+
+    .sdg-vis {
+      height: 100%;
+
+      line {
+        stroke: black;
+        stroke-width: 2px;
+      }
+    }
+  }
 </style>
 
