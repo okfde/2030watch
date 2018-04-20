2ee34a96f16d54d20487e7331ddd4845be4a462c
Jonas <jonasparnow@gmail.com>
2018-04-20 12:11:48 +0200

    clean up

--- pages/indicator/_indicator.vue
+++ pages/indicator/_indicator.vue
@@ -29,11 +29,7 @@
 <script>
   export default {
     validate ({ params, store }) {
-      // if this function returns false an 404 is printed
-      console.log(params)
-      // return true
       return store.getters.indicatorsSlugs.includes(params.indicator)
-      // return !params.slug || params.slug === 'active' || params.slug === 'completed'
     },
     head () {
       // console.log(this.editedTodo, this.project.title)
@@ -48,18 +44,12 @@
       }
     },
     mounted () {
-      // let self = this
-      // window.addEventListener('keyup', ev => {
-      //   self.$router.push('/')
-      // })
     },
     components: {
       // Slick
     },
     computed: {
       indicator () {
-        // This property gets calculated every time. It finds the corresponding project in the store
-        console.log(this.$route.params.indicator, this.$store.getters.indicators)
         return this.$store.getters.indicators[this.$route.params.indicator]
       }
     }
