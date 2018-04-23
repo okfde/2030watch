64130486d03be13639b158a7e807c5873670d10a
alsino <iamahorse@gmail.com>
2018-04-23 18:42:53 +0200

    aktiv content (raw)

--- pages/aktiv.vue
+++ pages/aktiv.vue
@@ -2,6 +2,45 @@
   <div class="container rows">
     <section>
       <h2>Werde aktiv</h2>
+      <div>
+        Wie nachhaltig ist Deutschland? 
+
+“Deutschland hat noch großen Handlungsbedarf um die Ziele der Agenda 2030 ambitioniert umsetzen.”  
+
+----------------------
+
+Infos und Anlaufpunkte
+
+Hier kannst Du Dich weiter informieren um mehr zum Thema Nachhaltigkeit in Deutschland zu erfahren. 
+
+Die Deutsche Nachhaltigkeitsstrategie
+
+Ministerien und Ämter
+UBA
+etc 
+
+Zivilgesellschaftliche Organisationen 
+VENRO
+FUE 
+etc
+
+Links zu SDG Events
+17ziele.de
+
+UN
+
+------------------------
+
+Werde aktiv!
+
+Schreibe an Deinen Abgeordneten (Beispiel) 
+
+Halte ein Workshop zu den SDGs
+
+Teile unsere Message auf Twitter
+
+-----------------
+      </div>
     </section>
   </div>
 </template>
