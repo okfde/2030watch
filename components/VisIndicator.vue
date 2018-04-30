c7dc73e453ed223f45ea1a49e2a18b0bcb3289f1
Jonas <jonasparnow@gmail.com>
2018-04-30 20:15:08 +0200

    VisIndicator basics

--- components/VisIndicator.vue
+++ components/VisIndicator.vue
@@ -1,7 +1,17 @@
 <template>
   <nuxt-link :to="'../indicator/' + i.slug">
     <div class="vis-indicator" ref="vis">
-      {{ i.label }}
+      <ul>
+        <li>{{ i.id }}</li>
+        <li>{{ i.label }}</li>
+        <li>{{ i.progress }}</li>
+        <li>badTarget: {{ i.badTarget ? 'ja' : 'nein' }}</li>
+        <li>badIndicator: {{ i.badIndicator ? 'ja' : 'nein' }}</li>
+        <li>uncalculable: {{ i.uncalculable ? 'ja' : 'nein' }}</li>
+        <li>spillover: {{ i.spill ? 'ja' : 'nein' }}</li>
+        <li>modTarget: {{ i.modTarget ? 'ja' : 'nein' }}</li>
+        <li>neuer: {{ i.altIndicator }}</li>
+      </ul>
     </div>
   </nuxt-link>
 </template>
@@ -33,6 +43,8 @@
   .vis-indicator {
     display: flex;
     width: 100%;
+
+    font-size: 0.7rem;
   }
 </style>
 
