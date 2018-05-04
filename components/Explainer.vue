0e46fddad9c695c4f228982ce39638c708e15c60
Jonas <jonasparnow@gmail.com>
2018-05-04 23:16:36 +0200

    headline for explainer

--- components/Explainer.vue
+++ components/Explainer.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="page-explainer">
-    <div class="wrapper columns columns-gutter">
+    <div class="wrapper">
+      <h3>2030 Watch in aller Kürze</h3>
+    </div>
+    <div class="faq wrapper columns columns-gutter">
       <section>
-        <small class="caption">2030 Watch &mdash; Was?</small>
+        <small class="caption">Was?</small>
         <p>Wir diskutieren wie nachhaltig Deutschland ist, indem wir überprüfen, inwieweit die Nachhaltigkeitsziele erreicht werden.</p>
       </section>
       <section>
-        <small class="caption">2030 Watch &mdash; Warum?</small>
+        <small class="caption">Warum?</small>
         <p>Wir vergleichen Deutschlands Fortschritt anhand von offiziellen und alternativen Indikatoren, denn die offiziellen Indikatoren sich nicht ambitioniert genug.</p>
       </section>
       <section>
-        <small class="caption">2030 Watch  &mdash; Wie?</small>
+        <small class="caption">Wie?</small>
         <p>Unsere Indikatoren zeigen fehlende Themen, bessere Zielwerte und mehr Verantwortung.</p>
       </section>
     </div>
@@ -23,6 +26,14 @@
 
 <style lang="scss" scoped>
   @import "~@/assets/style/variables";
+
+  h3 {
+    color: #fff;
+  }
+
+  .faq {
+    margin-top: $spacing;
+  }
 
   .page-explainer {
     background-color: rgba(0, 0, 0, 0.1);
