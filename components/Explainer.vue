e0561d1cc6ea5a32f277b16458993927b63c0a01
Jonas <jonasparnow@gmail.com>
2018-05-04 23:02:15 +0200

    explainer cleanup

--- components/Explainer.vue
+++ components/Explainer.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="page-explainer modern columns columns-gutter">
-    <div>
-      <small class="caption">2030 Watch &mdash; Was?</small>
-      <div>Wir diskutieren wie nachhaltig Deutschland ist, indem wir überprüfen, inwieweit die Nachhaltigkeitsziele erreicht werden.</div>
-    </div>
-    <div>
-      <small class="caption">2030 Watch &mdash; Warum?</small>
-      <div>Wir vergleichen Deutschlands Fortschritt anhand von offiziellen und alternativen Indikatoren, denn die offiziellen Indikatoren sich nicht ambitioniert genug.</div>
-    </div>
-    <div>
-      <small class="caption">2030 Watch  &mdash; Wie?</small>
-      <div>Unsere Indikatoren zeigen fehlende Themen, bessere Zielwerte und mehr Verantwortung.</div>
+  <div class="page-explainer">
+    <div class="wrapper columns columns-gutter">
+      <section>
+        <small class="caption">2030 Watch &mdash; Was?</small>
+        <p>Wir diskutieren wie nachhaltig Deutschland ist, indem wir überprüfen, inwieweit die Nachhaltigkeitsziele erreicht werden.</p>
+      </section>
+      <section>
+        <small class="caption">2030 Watch &mdash; Warum?</small>
+        <p>Wir vergleichen Deutschlands Fortschritt anhand von offiziellen und alternativen Indikatoren, denn die offiziellen Indikatoren sich nicht ambitioniert genug.</p>
+      </section>
+      <section>
+        <small class="caption">2030 Watch  &mdash; Wie?</small>
+        <p>Unsere Indikatoren zeigen fehlende Themen, bessere Zielwerte und mehr Verantwortung.</p>
+      </section>
     </div>
   </div>
 </template>
@@ -20,12 +22,9 @@
 </script>
 
 <style lang="scss" scoped>
-  @import "~@/assets/style/base";
+  @import "~@/assets/style/variables";
 
   .page-explainer {
-    font-size: 0.95rem;
-    background-color: #4ec8ff;
-    background-color: $color-mute;
-    padding: 3rem 15vw;
+    background-color: rgba(255, 255, 255, 0.1);
   }
 </style>
