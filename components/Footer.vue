41c76558261eb9152c967975fc45145e68df672e
Jonas <jonasparnow@gmail.com>
2018-05-10 13:50:32 +0200

    Semantic tags

--- components/Footer.vue
+++ components/Footer.vue
@@ -8,35 +8,33 @@
       Steuernummer: 27/674/52428<br />
       VAT: DE278022128
     </address>
-    <ul class="impressum">
+    <ul>
       <li><nuxt-link to="/impressum">Impressum</nuxt-link></li>
       <li><nuxt-link to="/kontakt">Kontakt</nuxt-link></li>
-      <li><a href="mailto:2030-watch@okfn.de">Feedback</a></li>
+      <li><a href="&#0109;&#097;&#0105;&#0108;&#0116;&#0111;&#058;&#050;&#048;&#051;&#048;&#045;&#0119;&#097;&#0116;&#099;&#0104;&#064;&#0111;&#0107;&#0102;&#0110;&#046;&#0100;&#0101;">Feedback</a></li>
     </ul>
-    <ul class="social">
-      <div><a href="http://okfn.us5.list-manage1.com/subscribe?u=929f1e07936386d34833e20d1&id=46ec58c60d">Newsletter abbonieren</a></div>
-      <div><a href="https://okfn.de/spenden/">Mit einer Spende unterstützen</a></div>
-      <br />
-      <div class="social-icons">
-        <a href="mailto:2030-watch@okfn.de"><img src="../assets/icons/email_light.png" alt=""></a>
-        <a href="https://twitter.com/2030watchde"><img src="../assets/icons/twitter_light.png" alt=""></a>
-        <a href="https://www.facebook.com/2030-Watch-108979439509739/"><img src="../assets/icons/fb_light.png" alt=""></a>
-        <a href="https://github.com/okfde/2030-watch.de"><img src="../assets/icons/gh_light.png" alt=""></a>
-      </div>
+    <ul>
+      <li><a href="http://okfn.us5.list-manage1.com/subscribe?u=929f1e07936386d34833e20d1&id=46ec58c60d">Newsletter abbonieren</a></li>
+      <li><a href="https://okfn.de/spenden/">Mit einer Spende unterstützen</a></li>
+      <li>
+        <ul class="social-icons">
+          <li><a href="mailto:2030-watch@okfn.de"><img src="../assets/icons/email_light.png" alt="Mail" /></a></li>
+          <li><a href="https://twitter.com/2030watchde"><img src="../assets/icons/twitter_light.png" alt="Twitter" /></a></li>
+          <li><a href="https://www.facebook.com/2030-Watch-108979439509739/"><img src="../assets/icons/fb_light.png" alt="Facebook" /></a></li>
+          <li><a href="https://github.com/okfde/2030-watch.de"><img src="../assets/icons/gh_light.png" alt="Github" /></a></li>
+        </ul>
+      </li>
     </ul>
   </footer>
 </template>
 
-<script>
-
-</script>
-
 <style lang="scss" scoped>
-  .page-footer {
-    .social {
-      .social-icons a {
-        margin-right: 2rem;
-      }
+  .social-icons {
+    margin-top: 1rem;
+
+    li {
+      display: inline-block;
+      margin-right: 2rem;
     }
   }
 </style>
