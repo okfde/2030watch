14d8770a1e71d5c28d64fa399528eabe57fc46bf
alsino <iamahorse@gmail.com>
2018-05-11 10:30:26 +0200

    wording

--- pages/index.vue
+++ pages/index.vue
@@ -19,7 +19,7 @@
         <h2>So nachhaltig ist Deutschland – Das sagen offizielle und komplementäre Daten</h2>
         <div class="columns columns-gutter">
           <section>
-            <p>2030 Watch stellt dem <span class="dns">offiziellen Indikatorenkatalog</span> der Bundesregierung einen <span class="okf">komplementären Indikatorenkatalog</span> entgegen, welcher den Fortschritt in Richtung Nachhaltigkeit misst. Die folgende Visualisierung vergleicht den bisher erreichten Fortschritt anhand beider Indikatorensets auf einer Skala von 0% (sehr geringe Nachhaltigkeit) bis 100% (sehr hohe Nachhaltigkeit).</p>
+            <p>2030 Watch stellt dem <span class="dns">offiziellen Indikatorenkatalog</span> der Bundesregierung einen <span class="okf">erweiterten Indikatorenkatalog</span> entgegen, welcher den Fortschritt in Richtung Nachhaltigkeit misst. Die folgende Visualisierung vergleicht den bisher erreichten Fortschritt anhand beider Indikatorensets auf einer Skala von 0% (sehr geringe Nachhaltigkeit) bis 100% (sehr hohe Nachhaltigkeit).</p>
           </section>
           <section>
             <VisProgress :sdg="sampleSDG" :vLegend="true" />
