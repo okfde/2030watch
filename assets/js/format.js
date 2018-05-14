b722800065978c05c668d1e3238b39b100ec32ea
Jonas <jonasparnow@gmail.com>
2018-05-14 12:31:28 +0200

    clean up

--- assets/js/format.js
+++ assets/js/format.js
@@ -4,7 +4,6 @@ module.exports = function format (v, d = 0, u = '%') {
   if (v === null || typeof v === 'undefined') {
     return '&mdash;'
   }
-  console.log(v)
   const str = d === 0 ? parseInt(v) : v.toFixed(d)
   const value = str.toString().replace('-', '–').replace('.', ',')
   return value + '&#8239;' + u
