68749deecfb2157ab599d23694d9327bc257d4fa
alsino <iamahorse@gmail.com>
2018-05-16 18:51:30 +0200

    Slideshow updated

--- components/Slideshow.vue
+++ components/Slideshow.vue
@@ -11,32 +11,59 @@
         <div v-else-if="slide === 2" ref="slide2">
           <h5 class="caption">Ein Beispiel: SDG 5 – Geschlechtergerechtigkeit</h5>
           <p>Das Nachhaltigkeitsziel SDG 5 steht für die Gleichstellung der Geschlechter. Im Zuge der gesetzlichen Vorschriften sind bestimmte Unternehmen in Deutschland seit 2016 dazu verpflichtet, mindestens 30% Frauenanteil in den Aufsichtsräten zu erreichen. Auch die Bundesregierung hat dasselbe Ziel für SDG 5 gesetzt, jedoch bis 2030. Die Vereinten Nationen sprechen von gleichberechtigter Partizipation von Frauen in Politik, Wirtschaft und öffentlichem Leben.</p>
-          <p>Aus Sicht von 2030 Watch ist daher das Ziel von 30% der Deutschen Nachhaltigkeitsstrategie (DNS) nicht ambitioniert genug. Mit der Betrachtung von lediglich 106 Unternehmen nimmt die DNS außerdem nur einen unzureichenden Bereich der Teilhabe in den Blick.</p>
         </div>
         <div v-else-if="slide === 3" ref="slide3">
           <p>Aus Sicht von 2030 Watch ist daher das Ziel von 30% der Deutschen Nachhaltigkeitsstrategie (DNS) nicht ambitioniert genug. Mit der Betrachtung von lediglich 106 Unternehmen nimmt die DNS außerdem nur einen unzureichenden Bereich der Teilhabe in den Blick.</p>
+          <p>Die Konsequenz: 2030 Watch fordert im Einklang mit SDG 5 einen Frauenanteil von 50% in Aufsichtsräten und anderen Führungsebenen. Ergänzend zur Wirtschaft ist 2030 Watch das Thema der politischen Partizipation von Frauen ebenso wichtig. Daher ergänzt 2030 Watch die DNS um die Betrachtung des Frauenanteils in Parlamenten. Auch beim Anteil von Frauen in Parlamenten wird ein starker Handlungsbedarf Deutschlands offensichtlich.</p>
         </div>
         <div v-else ref="slide4">
-          <p>Die Konsequenz: 2030 Watch fordert im Einklang mit SDG 5 einen Frauenanteil von 50% in Aufsichtsräten und anderen Führungsebenen. Ergänzend zur Wirtschaft ist 2030 Watch das Thema der politischen Partizipation von Frauen ebenso wichtig. Daher ergänzt 2030 Watch die DNS um die Betrachtung des Frauenanteils in Parlamenten. Auch beim Anteil von Frauen in Parlamenten wird ein starker Handlungsbedarf Deutschlands offensichtlich.</p>
           <p>Viele weitere spannende Stories und Ergänzen finden sich in den weiteren SDGs. Lernt, diskutiert, entdeckt!</p>
         </div>
       </div>
       <div class="indicators">
           <div v-if="slide === 1" ref="slide1" class="legendProgress">
             <div class="sdg-process-wrapper">
-              <VisProgress :sdg="sampleSDG" :vLegend="true" :compact="false" />
+              <VisProgress :sdg="introSDG" :vLegend="true" :compact="false" />
             </div>
           </div>
           <div v-else-if="slide === 2" ref="slide2">
-            <h5 class="caption">Offizielle DNS Indikatoren SDG 5</h5>
-            <ul>
-              <li v-for="item in indicators_dns "><VisIndicator :i="item" :compact="true" color="F8B300" /></li>
+            <h5 class="caption">Offizielle DNS Indikatoren – SDG 5</h5>
+            <ul class="indicator-list">
+              <li v-for="indicator in indicators_dns ">
+                <VisIndicator :i="indicator" :compact="true" :tiny="true" color="F8B300" />
+              </li>
             </ul>
-            <h5 class="caption">Erweiterte 2030 Watch Indikatoren SDG 5</h5>
-            <ul>
-               <li v-for="item in indicators_okf "><VisIndicator :i="item" :compact="true" color="04A6F0" /></li>
-            </ul>
+            <h5 class="caption">Offizieller Fortschritt – SDG 5</h5>
+             <div class="sdg-process-wrapper">
+              <VisProgress :sdg="officialSDG" 
+              :vLegend="false" 
+              :compact="false" 
+              :invert="true" />
+            </div>
+            <!-- <ul class="indicator-list">
+              <li v-for="indicator in indicators_dns ">
+                <VisIndicator :i="indicator" :compact="true" :tiny="true" color="F8B300" />
+              </li>
+            </ul> -->
           </div>
+          <div v-else-if="slide === 3" ref="slide3">
+            <h5 class="caption">Erweiterte 2030 Watch Indikatoren – SDG 5</h5>
+            <ul class="indicator-list">
+              <li v-for="indicator in indicators_okf ">
+                 <VisIndicator :i="indicator" :compact="true" :tiny="true" color="04A6F0" />
+              </li>
+            </ul>
+            <!-- <h5 class="caption">2030 Watch Fortschritt – SDG 5</h5>
+            <div class="sdg-process-wrapper">
+              <VisProgress :sdg="officialSDG" 
+                :vLegend="false" 
+                :compact="false" 
+                :invert="true" />
+          </div> -->
+          </div>
+
+            
+         
       </div>
     </section>
     <footer>
@@ -62,10 +89,16 @@
     data: function () {
       return {
         slide: 1,
-        sampleSDG: {
+        introSDG: {
           values: {
             okf: 48,
             dns: 89
+          }
+        },
+        officialSDG: {
+          values: {
+            okf: 0,
+            dns: 34
           }
         }
       }
@@ -106,8 +139,15 @@
   @import "~@/assets/style/variables";
 
   .intro {
+    position: relative;
+    height: 70vh;
+
     header, section, footer {
       width: 100%;
+    }
+
+    .caption {
+      margin-bottom: 0.5rem;
     }
 
     .dns {
@@ -127,9 +167,8 @@
         height: 100%;
       }
 
-      ul {
+      .indicator-list {
         padding: $spacing/2 0;
-        text-align: center;
 
         li {
           display: inline-block;
@@ -139,7 +178,10 @@
 
     footer {
       text-align: right;
-      margin-top: $spacing;
+      margin-top: $spacing/2;
+      position: absolute;
+      bottom: 1rem;
+      left: 0;
 
       .progress {
         text-align: center;
@@ -152,11 +194,10 @@
           margin-right: 10px;
 
           &.past {
-            background-color: $color-okf;
+            background-color: #FCEA2B;
           }
 
           &.future {
-            // background-color: lighten($color-mute, 20%);
             background-color: rgba(0, 0, 0, .1);
           }
         }
@@ -166,8 +207,8 @@
         display: inline-block;
         margin: 0 $spacing / 2;
 
-        &:last-child {
-          margin-right: 0;
+        &:last-of-type {
+          margin-right: $spacing * 2;
         }
       }
     }
