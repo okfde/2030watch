756fa4f9aedb7e68a3d99d58b43db5a2d4e43b10
Jonas <jonasparnow@gmail.com>
2018-05-18 10:40:21 +0200

    clean up

--- pages/samplepost.vue
+++ pages/samplepost.vue
@@ -50,13 +50,8 @@
 
 <script>
   import BlogpostPreview from '~/pages/blog/Blogpost-Preview.vue'
+
   export default {
-    computed: {
-    },
-    methods: {
-    },
-    directives: {
-    },
     components: {
       BlogpostPreview
     }
