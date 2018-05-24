f94a0fe68e30d64efb153ea9ed9ee3048012b6cb
Jenny Gebske <jennifer@gebske.de>
2018-05-24 11:07:34 +0200

    remove OKF

--- pages/sdg/_sdg.vue
+++ pages/sdg/_sdg.vue
@@ -37,7 +37,7 @@
             </div>
             <div>
               <hgroup>
-                <h3 class="okf">2030Watch (OKF)</h3>
+                <h3 class="okf">2030Watch</h3>
               </hgroup>
               <p>2030 Watch schlägt hingegen eine erweiterte Indikatoren-Liste vor, die {{ numberToStringNominativ(sdg.n.udns) }} offizielle{{ sdg.n.udns <= 1 ? 'n' : '' }} Indikator{{ sdg.n.udns <= 1 ? '' : 'en' }} übernimmt, {{ numberToStringNominativ(sdg.n.baI) }} streicht, {{ numberToStringNominativ(sdg.n.moT) }} modifiziert sowie {{ sdg.n.okf }} weitere{{ sdg.n.okf <= 1 ? 'n' : '' }} hinzufügt. Aus diesem alternativen Indikatorenset würde sich folgender Fortschritt bei »{{ sdg.labelShort }}« berechnen:</p>
               <span class="process okf" v-html="format(okf)" />
