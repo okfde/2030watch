11f7c6f2f9c2facfe20b924c49dc42104a0ae3e2
Jenny Gebske <jennifer@gebske.de>
2018-05-30 18:26:07 +0200

    remove space

--- components/Footer.vue
+++ components/Footer.vue
@@ -11,7 +11,6 @@
         <li>
           Wir freuen uns über <a title="Sende uns per E-Mail dein Feedback" class="footer-link" href="&#0109;&#097;&#0105;&#0108;&#0116;&#0111;&#058;&#050;&#048;&#051;&#048;&#045;&#0119;&#097;&#0116;&#099;&#0104;&#064;&#0111;&#0107;&#0102;&#0110;&#046;&#0100;&#0101;">Feedback</a>
         </li>
-
         <li><a href="https://okfn.de/spenden/">Mit einer Spende unterstützen</a></li>
         <li>
           <ul class="social-icons">
