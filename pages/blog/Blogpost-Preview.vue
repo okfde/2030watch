69f913483b0aa0d92145b2237b0e9d7d3c30d6c0
Jenny Gebske <jennifer@gebske.de>
2018-06-05 22:26:09 +0200

    adjust link

--- pages/blog/Blogpost-Preview.vue
+++ pages/blog/Blogpost-Preview.vue
@@ -7,13 +7,13 @@
     </div>
     <div class="post-teaser">
       <span class="caption">6. Juni 2018</span>
-      <nuxt-link to="/nachhaltigkeit">
+      <nuxt-link to="/blog-post">
         <h3>Gleise auf Nachhaltigkeit umstellen – wie weit ist der Weg eigentlich noch?</h3>
       </nuxt-link>
       <div>
       </div>
       Die letzte Woche nahm Nachhaltigkeit in Deutschland unter die Lupe. Ein internationales Experten Gremium stellte gestern einen <a href="https://www.nachhaltigkeitsrat.de/projekte/peer-review/" target="_blank">Bericht</a> mit der Schlussfolgerung, dass die Deutsche Nachhaltigkeitspolitik noch nicht nachhaltig genug ist, vor. Gleichzeitig engagierten sich so viele Menschen wie nie zuvor bei den deutschen Aktionstagen Nachhaltigkeit.
-      <nuxt-link to="/nachhaltigkeit"><span>Mehr lesen …</span></nuxt-link>
+      <nuxt-link to="/blog-post"><span>Mehr lesen …</span></nuxt-link>
     </div>
   </div>
 </template>
