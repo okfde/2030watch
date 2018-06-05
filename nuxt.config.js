ebe2b5ef3015aa543705bf649fa7ef0241bf08a4
Jenny Gebske <jennifer@gebske.de>
2018-06-05 11:25:28 +0200

    adjust path to font and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: '2030Watch diskutiert wie ambitioniert Deutschland die Nachhaltigkeitsziele der Agenda 2030 umsetzt.' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
+      { rel: 'icon', type: 'image/x-icon', href: '~static/favicon.png' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Chivo:400,700|Roboto+Mono:400,700', defer: true }
     ]
   },
