eab6bba65c1a6de9cff27a4a7e76e6c12bfc4beb
Jenny Gebske <jennifer@gebske.de>
2018-06-11 13:01:19 +0200

    update blog post

--- pages/blog-post.vue
+++ pages/blog-post.vue
@@ -27,20 +27,21 @@
         </div>
       </header>
       <section class="blog-body wrapper">
+
         <div>
-          Die letzte Woche nahm Nachhaltigkeit in Deutschland unter die Lupe. Ein internationales Experten Gremium stellte gestern einen <a href="https://www.nachhaltigkeitsrat.de/projekte/peer-review/" target="_blank">Bericht</a> mit der Schlussfolgerung, dass die Deutsche Nachhaltigkeitspolitik noch nicht nachhaltig genug ist, vor. Gleichzeitig engagierten sich so viele Menschen wie nie zuvor bei den deutschen Aktionstagen Nachhaltigkeit. Klar ist, um ein nachhaltigeres Deutschland kommen wir nicht herum, aber wie (ehrgeizig) wir die Transformation angehen steht zur Debatte.
+          Die Thema Nachhaltigkeit wurde letzte Woche mehrmals in Deutschland unter die Lupe genommen. Ein internationales Experten*innen-Gremium stellte gestern einen <a href="https://www.nachhaltigkeitsrat.de/projekte/peer-review/" target="_blank">Bericht</a> (Peer Review) vor. Die Expert*innen kommen darin zu dem Schluss, dass die Deutsche Nachhaltigkeitspolitik bisher noch nicht genügend greift. Gleichzeitig engagierten sich so viele Menschen wie nie zuvor bei den deutschen Aktionstagen "Taten für morgen" zum Thema Nachhaltigkeit. Klar ist, dass wir auf dem Weg zu einem nachhaltigeren Deutschland sind, jedoch nicht ausreichend viele Gleise umgestellt sind. Es steht also zur Debatte, wie (ehrgeizig) wir diese Transformation angehen.
         </div>
 
         <br />
 
         <div>
-          Mit unserem neuen Indikatorenkatalog will 2030Watch ein Werkzeug bieten, um die Nachhaltigkeitsagenda in Deutschland kritisch zu untersuchen. Wie wird Nachhaltigkeit in Deutschland eigentlich gemessen? Was könnte man messen? Was sollte man messen?
+          Mit unserem neuen Indikatorenkatalog möchte 2030Watch ein Werkzeug bieten, um die Nachhaltigkeitsagenda in Deutschland kritisch zu untersuchen. <i>Wie wird Nachhaltigkeit in Deutschland eigentlich gemessen? Was könnte man messen? Was sollte man messen?</i>
         </div>
 
         <br />
 
         <div>
-          2030Watch untersucht anhand von Indikatoren, wie ambitioniert Deutschland die globalen Nachhaltigkeitsziele umsetzt. Die 2030Watch Indikatoren zeigen, dass der Handlungsbedarf in Deutschland noch viel größer ist als was die Bundesregierung sich zum Ziel gemacht hat. Zum Beispiel bei <nuxt-link :to="'../sdg/gleichstellung'">SDG 5</nuxt-link> – Geschlechtergerechtigkeit – hat sich die Bundesregierung ein Ziel bei <nuxt-link :to="'../indicator/dns-frauen-wirtschaft-dns'">Frauen in Führungspositionen</nuxt-link> für 2030 gesetzt, dass seit 2016 schon gesetzlich Pflicht ist. 2030Watch fordert einen höheren Zielwert und benennt damit einen größeren Handlungsbedarf.
+          2030Watch untersucht anhand von Indikatoren, wie ambitioniert Deutschland die globalen Nachhaltigkeitsziele umsetzt. Die 2030Watch Indikatoren zeigen, dass der Handlungsbedarf in Deutschland noch viel größer ist, als es sich die Bundesregierung zum Ziel gemacht hat. Zum Beispiel hat sich die Bundesregierung bei <nuxt-link :to="'../sdg/gleichstellung'">SDG 5</nuxt-link> - Geschlechtergerechtigkeit - ein Ziel bei <nuxt-link :to="'../indicator/dns-frauen-wirtschaft-dns'">Frauen in Führungspositionen</nuxt-link> für 2030 gesetzt, dass aber seit 2016 bereits gesetzliche Pflicht ist. 2030Watch bewertet daher dieses Ziel als wenig ambitioniert und fordert einen höheren Zielwert. Damit benennen wir einen größeren Handlungsbedarf.
         </div>
 
         <br />
@@ -52,19 +53,19 @@
         <br />
 
         <div>
-          Am gestrigen Weltumwelttag (5. Juni) wurde das Thema Plastikmüll von der UNO mit einem neuen Bericht hervorgehoben; ein <a href="http://www.spiegel.de/video/weltumwelttag-2018-wie-laesst-sich-plastik-vermeiden-video-99017888.html" target="_blank">Spiegel Online</a> Beitrag zitiert "Machen wir so weiter wie bisher, wiegt der Plastikmüll im Meer im Jahr 2050 genauso viel wie alle Fische zusammen", neben der EU Plastikstrategie 2018 fordert auch das <a href="http://www.bmz.de/de/presse/aktuelleMeldungen/2018/juni/180605_Weltumwelttag_Mueller_fordert_Kampf_gegen_Plastikmuell/" target="_blank">BMZ</a> einen Kampf gegen Plastikmüll. Warum fehlt dieses Thema in der Deutschen Nachhaltigkeitsstrategie?
+          Am gestrigen Weltumwelttag (5. Juni) wurde durch die UN das drängende Thema Plastikmüll  mit einem neuen Bericht hervorgehoben; Ein <a href="http://www.spiegel.de/video/weltumwelttag-2018-wie-laesst-sich-plastik-vermeiden-video-99017888.html" target="_blank">Spiegel Online</a>-Beitrag zitiert den Leiter des UNO-Umweltprogramms Erik Solheim: "Machen wir so weiter wie bisher, wiegt der Plastikmüll im Meer im Jahr 2050 genauso viel wie alle Fische zusammen." Neben der EU Plastikstrategie 2018 fordert auch das <a href="http://www.bmz.de/de/presse/aktuelleMeldungen/2018/juni/180605_Weltumwelttag_Mueller_fordert_Kampf_gegen_Plastikmuell/" target="_blank">BMZ</a> einen Kampf gegen Plastikmüll. Warum fehlt dieses Thema in der Deutschen Nachhaltigkeitsstrategie?
         </div>
 
         <br />
 
         <div>
-          Stattdessen hat sich die Bundesregierung bei <nuxt-link :to="'../sdg/konsum'">SDG 12</nuxt-link> – Nachhaltiger Konsum und Produktion – zum Ziel gesetzt, Konsum von Nachhaltigen Produkten zu erhöhen. 2030Watch fordert ein Umdenken weg von besserem Konsum auf weniger Konsum und zieht hierzu Indikatoren zu <nuxt-link :to="'../indicator/okf-verpackungsmuell'">Verpackungsmüll</nuxt-link> und <nuxt-link :to="'../indicator/okf-wasserflaschen-konsum'">Verbrauch von Wasserflaschen</nuxt-link> hinzu um auf die untragbaren Umweltkosten hinzuweisen.
+          Stattdessen hat sich die Bundesregierung bei <nuxt-link :to="'../sdg/konsum'">SDG 12</nuxt-link> - Nachhaltiger Konsum und Produktion - zum Ziel gesetzt, Konsum von Nachhaltigen Produkten zu erhöhen, ohne auf die allgemeine Plastikverbräuche einzugehen. 2030Watch fordert ein Umdenken weg vom besseren Konsum zu weniger Konsum und zieht hierzu Indikatoren zu <nuxt-link :to="'../indicator/okf-verpackungsmuell'">Verpackungsmüll</nuxt-link> und <nuxt-link :to="'../indicator/okf-wasserflaschen-konsum'">Verbrauch von Wasserflaschen</nuxt-link> hinzu, um auf die untragbaren Umweltkosten hinzuweisen.
         </div>
 
         <br />
 
         <div>
-          Fazit, um Nachhaltigkeit ambitioniert umzusetzen brauchen wir ehrgeizige Indikatoren.
+          Fazit, um Nachhaltigkeit ambitioniert umzusetzen, brauchen wir ehrgeizige Indikatoren.
         </div>
 
         <br />
@@ -77,10 +78,10 @@
             Welche Indikatoren finden sich bei der Deutschen Nachhaltigkeitsstrategie (DNS)?
           </li>
           <li>
-            Wie gut sind deren Ziele?
+            Wie treffend sind deren Zielwerte?
           </li>
           <li>
-            Wie könnte man die Ziele besser messen?
+            Wie könnte man die Zielerreichung besser messen?
           </li>
           <li>
             Welche Themen fehlen?
@@ -90,17 +91,11 @@
           </li>
         </ul>
 
-        <!-- <h4>
-          Außerdem kannst du bei uns
-        </h4>
-        <ul>
-          <li>
-            Daten zu Indikatoren herunterladen
-          </li>
-          <li>
-            Graphiken herunterladen und teilen
-          </li>
-        </ul> -->
+        <div>
+          Außerdem kannst Du bei uns die Daten zu den Indikatoren sowie bald auch die dazugehörigen Grafiken herunterladen und teilen.
+        </div>
+
+        <br />
 
         <div>
           Über den Sommer bauen wir 2030Watch noch weiter aus. Neben den Inhalten zu allen SDGs, freuen wir uns auch über Feedback zu anderen gewünschten Features und auch über Organisationen, die bei uns als Datenpate einen Indikator adoptieren oder einbringen möchten.
@@ -115,7 +110,7 @@
         <br />
 
         <div>
-          *Nachwort: Übrigens haben wir bisher nur die Inhalte zu SDG 2, SDG 5, SDG 9, SDG 12, SDG 16 live, aber in den nächsten Wochen werden Indikatoren zu allen SDGs hochgeladen sein.
+          *Nachwort: Übrigens noch haben wir bisher die Inhalte zu SDG 2, SDG 5, SDG 9, SDG 12, SDG 16 live, in den nächsten Wochen werden Indikatoren zu allen SDGs hochgeladen sein.
         </div>
       </section>
 
