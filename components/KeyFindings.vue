f1fdb1733ecaac6487aa83fd94b8a4cb66b1c065
Jenny Gebske <jennifer@gebske.de>
2018-07-18 13:34:10 +0200

    adjust sustainability legend

--- components/KeyFindings.vue
+++ components/KeyFindings.vue
@@ -12,44 +12,31 @@
         <tr>
           <th>Nachhaltigkeit<span class="mobile-invisible">sstufe</span></th>
           <th class="mobile-invisible">SDG-Ziel erreicht zu</th>
-          <th><span class="mobile-invisible">Anzahl &ndash; </span>DNS</th>
+          <th><span class="mobile-invisible">Anzahl &ndash; </span>Offiziell</th>
           <th><span class="mobile-invisible">Anzahl &ndash; </span>2030Watch</th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td><span class="icon mobile-invisible" :style="{ 'background-color': stepsColors[4] }" /> sehr hoch</td>
-          <td v-html="format(80) + '&#8239;&mdash;&#8239;' + format(100)" class="mobile-invisible">
-          <td><span class="icon dns" v-for="item in countingsDNS['sehr hohe Nachhaltigkeit']" /></td>
-          <td><span class="icon okf" v-for="item in countingsOKF['sehr hohe Nachhaltigkeit']" /></td>
-        </tr>
-        <tr>
-          <td><span class="icon mobile-invisible" :style="{ 'background-color': stepsColors[3] }" /> hoch</td>
-          <td v-html="format(60) + '&#8239;&mdash;&#8239;' + format(80)" class="mobile-invisible">
-          <td><span class="icon dns" v-for="item in countingsDNS['hohe Nachhaltigkeit']" /></td>
-          <td><span class="icon okf" v-for="item in countingsOKF['hohe Nachhaltigkeit']" /></td>
-        </tr>
-        <tr>
-          <td><span class="icon mobile-invisible" :style="{ 'background-color': stepsColors[2] }" /> mittel</td>
-          <td v-html="format(40) + '&#8239;&mdash;&#8239;' + format(60)" class="mobile-invisible">
-          <td><span class="icon dns" v-for="item in countingsDNS['mittlere Nachhaltigkeit']" /></td>
-          <td><span class="icon okf" v-for="item in countingsOKF['mittlere Nachhaltigkeit']" /></td>
-        </tr>
-        <tr>
-          <td><span class="icon mobile-invisible" :style="{ 'background-color': stepsColors[1] }" /> niedrig</td>
-          <td v-html="format(20) + '&#8239;&mdash;&#8239;' + format(40)" class="mobile-invisible">
+          <td><span class="icon mobile-invisible" :style="{ 'background-color': stepsColors[0] }" /> niedrig</td>
+          <td v-html="'&ensp;' + format(0) + '&#8239;&mdash;&#8239;' + format(40)" class="mobile-invisible">
           <td><span class="icon dns" v-for="item in countingsDNS['geringe Nachhaltigkeit']" /></td>
           <td><span class="icon okf" v-for="item in countingsOKF['geringe Nachhaltigkeit']" /></td>
         </tr>
         <tr>
-          <td><span class="icon mobile-invisible" :style="{ 'background-color': stepsColors[0] }" /> sehr niedrig</td>
-          <td v-html="'&ensp;' + format(0) + '&#8239;&mdash;&#8239;' + format(20)" class="mobile-invisible">
-          <td><span class="icon dns" v-for="item in countingsDNS['sehr geringe Nachhaltigkeit']" /></td>
-          <td><span class="icon okf" v-for="item in countingsOKF['sehr geringe Nachhaltigkeit']" /></td>
+          <td><span class="icon mobile-invisible" :style="{ 'background-color': stepsColors[1] }" /> mittel</td>
+          <td v-html="format(40) + '&#8239;&mdash;&#8239;' + format(80)" class="mobile-invisible">
+          <td><span class="icon dns" v-for="item in countingsDNS['mittlere Nachhaltigkeit']" /></td>
+          <td><span class="icon okf" v-for="item in countingsOKF['mittlere Nachhaltigkeit']" /></td>
+        </tr>
+        <tr>
+          <td><span class="icon mobile-invisible" :style="{ 'background-color': stepsColors[2] }" /> hoch</td>
+          <td v-html="format(80) + '&#8239;&mdash;&#8239;' + format(100)" class="mobile-invisible">
+          <td><span class="icon dns" v-for="item in countingsDNS['hohe Nachhaltigkeit']" /></td>
+          <td><span class="icon okf" v-for="item in countingsOKF['hohe Nachhaltigkeit']" /></td>
         </tr>
       </tbody>
     </table>
-    <!-- <p>Den größten Handlungsbedarf sehen wir bei den Indikatoren »{{ first.label }}« und »{{ last.label }}«. Dort sind ist der Fortschritt zwischen dem offiziellem und dem inoffiziellem Indikatorenset am größten (<span v-html="format(first.diff)" />/<span v-html="format(last.diff)" />).</p> -->
   </div>
 </template>
 
