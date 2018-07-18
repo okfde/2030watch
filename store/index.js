796a6bb941c084caeb633cd4bacc8c0ca7be793e
Jenny Gebske <jennifer@gebske.de>
2018-07-18 13:31:53 +0200

    adjust color steps

--- store/index.js
+++ store/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import chroma from 'chroma-js'
 import * as data from '../data/indicators.json'
 import * as dataSDGs from '../data/sdgs.json'
 import _ from 'lodash'
@@ -29,12 +28,13 @@ const store = () => new Vuex.Store({
       return _.fromPairs(values)
     },
     stepsColors (state) {
-      const scale = chroma.scale(['#D22F27', '#F1B31C', '#5C9E31']).mode('lab').domain([0, state.steps])
-      const steps = []
-      for (let i = 0; i < state.steps; i++) {
-        steps.push(scale(i).hex())
-      }
-      return steps
+      // const scale = chroma.scale(['#D22F27', '#F1B31C', '#5C9E31']).mode('lab').domain([0, state.steps])
+      // const steps = []
+      // for (let i = 0; i < state.steps; i++) {
+      //   steps.push(scale(i).hex())
+      // }
+      // return steps
+      return ['#D22F27', '#F1B31C', '#5C9E31']
     },
     sdgsSlugs (state, getters) {
       return Object.keys(state.sdgs)
