0a7d5361fe0da288b9acbf44b814466c4add884b
Jenny Gebske <jennifer@gebske.de>
2018-08-07 18:34:12 +0200

    add reactivity for bar-chart

--- components/BarChart.js
+++ components/BarChart.js
@@ -1,9 +1,11 @@
-import { Bar } from 'vue-chartjs'
+import { Bar, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
 
 export default {
   extends: Bar,
-  props: ['data', 'options'],
+  mixins: [reactiveProp],
+  props: ['options'],
   mounted () {
-    this.renderChart(this.data, this.options)
+    this.renderChart(this.chartData, this.options)
   }
 }
