0147381bf05feb3e206e72e071b102a00a78d43f
Jenny Gebske <jennifer@gebske.de>
2018-08-27 15:46:37 +0200

    add method page

---
+++ pages/methodik.vue
@@ -0,0 +1,173 @@
+<template>
+  <div class="page-container">
+    <header class="inpage-header">
+      <div class="wrapper">
+        <h1>Methodik</h1>
+        <p>Hier findest du Informationen zu unserer Methodik</p>
+      </div>
+    </header>
+    <div class="content">
+      <div id="methode" class="wrapper">
+
+        <section>
+          <h2>Welche Indikatoren werden dargestellt?</h2>
+          <p>
+            Die offiziellen Indikatoren wurden ausschließlich von der Deutschen Nachhaltigkeitsstrategie (DNS) übernommen.
+            Die Deutsche Nachhaltigkeitsstrategie beurteilt die Indikatoren mit einem Wetterzeichen und zeigt so den Trend,
+            ob sich der Indikator dem Zielwert nähert oder nicht. Wir von 2030Watch haben die Indikatoren in ein eigenes
+            Bewertungssystem übersetzt, welches versucht, darzustellen, wie weit ein Indikator vom Ziel für 2030 entfernt ist.
+          </p>
+          <p>
+            Auf der 2030Watch Seite werden hierzu weitere Indikatoren hinzugezogen, welche von der Zivilgesellschaft als
+            Fortschrittsmaßstäbe von Nachhaltigkeit vorgeschlagen werden.
+          </p>
+        </section>
+
+        <section>
+          <h2>Wie hat 2030Watch Indikatoren ausgewählt?</h2>
+          <p>
+            Die ergänzenden Indikatoren hat 2030Watch geprüft und nach eigener Bewertung ausgewählt. Die Auswahl beruht auf unserer Recherche, Indikatorvorschlägen nichtstaatlicher Akteure und auf Gesprächen mit Experten aus Zivilgesellschaft und Forschung.
+          </p>
+          <p>Unsere Auswahlkriterien sind:</p>
+          <ol>
+            <li>
+              Das SDG Unterziel ist für Deutschland relevant.
+            </li>
+            <li>
+              Deutschland hat aus offizieller Sicht zu diesem Unterziel Handlungsbedarf. Handlungsbedarf besteht,
+              wenn eines der folgenden Kriterien erfüllt ist.
+              <ol>
+                <li style="list-style-type: lower-latin;">Es gibt einen offiziellen IAEG-SDG (Inter-Agency and Expert Group on Sustainable Development Goal) oder EU2020 Indikator, bei dem Deutschland mehr als 20% vom Zielwert entfernt ist.</li>
+                <li style="list-style-type: lower-latin;">Die Bundesregierung bezeichnet dieses Unterziel in der Nachhaltigkeitsstrategie als relevant, hat aber keinen eigenen Indikator.</li>
+                <li style="list-style-type: lower-latin;">Die Bundesregierung bezeichnet dieses Ziel selbst in offiziellen Verlautbarungen (inkl. internationalen Abkommen) als relevant, hat aber keinen eigenen Indikator.</li>
+              </ol>
+            </li>
+            <li>
+              Deutschland hat aus zivilgesellschaftlicher Sicht zu diesem Unterziel großen Handlungsbedarf.
+              In Kommentaren von respektierten Organisationen wird ein Indikator zu diesem Thema gefordert.
+            </li>
+            <li>
+              Disaggregierte Daten "Leave no one behind"
+              <ol>
+                <li style="list-style-type: lower-latin;">Gender</li>
+                <li style="list-style-type: lower-latin;">Kinder, Jugendliche, Erwachsene und Senioren</li>
+                <li style="list-style-type: lower-latin;">Menschen mit Migrationshintergrund</li>
+                <li style="list-style-type: lower-latin;">Menschen mit Behinderungen</li>
+              </ol>
+            </li>
+            <li>Vollständigkeit der Daten: Daten sind für Deutschland verfügbar, werden regelmäßig erhoben und sind in 2013 oder später erhoben worden.</li>
+            <li>Datenqualität: Daten stammen von offiziellen statistischen Quellen. Die Bewertungsmethode ist idealerweise an internationalen Vereinbarungen – good practices – angelehnt.</li>
+          </ol>
+        </section>
+
+        <section>
+          <h2>Wie hat 2030Watch die Indikatoren eingeteilt (Kategorien)?</h2>
+          <p>Indikatoren wurden in folgende Kategorien eingeteilt:</p>
+          <p><span style="text-decoration:underline;">Neues Thema:</span> Dieser Indikator legt ein neues Thema vor, das noch nicht in der Deutschen Nachhaltigkeitsstrategie (DNS) beinhaltet ist.</p>
+          <p><span style="text-decoration:underline;">Ungeeignet:</span>  Der Indikator misst keine Veränderung in Bezug auf Nachhaltigkeit und misst die Wirkung einer Maßnahme nur unzureichend.</p>
+          <p><span style="text-decoration:underline;">Übernommen:</span> Dieser Indikator ist ein offizieller Indikator der deutschen Nachhaltigkeitsstrategie (DNS) und wird von 2030Watch ohne Veränderungen übernommen.</p>
+          <p><span style="text-decoration:underline;">Nicht bewertbarer Zielwert:</span> Dieser Indikator hat keinen genau definierten Zielwert und erlaubt es daher nicht, den Fortschritt bis 2030 zu bewerten.</p>
+          <p><span style="text-decoration:underline;">Modifizierter Zielwert:</span> Dieser Zielwert ist unzureichend und 2030Watch schlägt einen neuen Zielwert vor.</p>
+          <p><span style="text-decoration:underline;">Nicht berechenbar:</span> Für diesen Indikator kann man auf Grund fehlender Daten den Ziel- oder Ausgangswert nicht berechnen.</p>
+          <p><span style="text-decoration:underline;">Internationale Auswirkungen:</span> Dieser Indikator misst internationale Auswirkungen durch Deutschland und zeigt somit die Verantwortung Deutschlands für nachhaltige Rahmenbedingungen in anderen Ländern.</p>
+        </section>
+
+        <section>
+          <h2>Wie misst 2030Watch den Fortschritt eines Indikators?</h2>
+          <p>
+            Wir berechnen für jeden Indikator, wie weit er vom jeweiligen Zielwert entfernt ist. Der jeweilige Zielwert wird dann als 100% eingestuft.
+            Die Formel um den Fortschritt zu berechnen ist:
+            Ausgangswert-Aktueller Wert/Ausgangswert-Zielwert.
+            So wird eine Vergleichbarkeit der Fortschritte zwischen den einzelnen Indikatoren hergestellt.
+          </p>
+        </section>
+
+        <section>
+          <h2>Wie misst 2030Watch den Fortschritt zur Erreichung von einem Nachhaltigkeitsziel?</h2>
+          <p>
+            Der Fortschritt auf Seiten der Deutschen Nachhaltigkeitsstrategie (DNS) wird mit einem Mittelwert
+            von allen Indikatorenwerten pro jeweiligem SDG berechnet.
+          </p>
+          <p>
+            Der Fortschritt auf der Seite von 2030Watch wird über den Mittelwert des jeweiligen erweiterten
+            Indikatorenkataloges beziffert. Indikatoren, die als ungeeignet eingestuft wurden,
+            fließen nicht in die Berechnung ein.
+          </p>
+          <p>
+            Wichtig zu betonen, ist, dass wir nicht behaupten, diese Fortschrittsberechnung sei eine objektive Darstellung.
+            Die Berechnungen sollen dazu dienen, einen Vergleich zwischen Nachhaltigkeitsbildern zu ermöglichen und
+            stellen einen Debattenbeitrag dar.
+          </p>
+        </section>
+
+        <section>
+          <h2>Wie werden Ausgangswerte berechnet?</h2>
+          <p>
+            Wir lehnen unsere Auswahl an gängige Vorgehen an, wie zum Beispiel das von
+            <a href="http://www.sdgindex.org/" target="_blank">Sustainable Development Solutions Network</a> (SDSN).
+            Sobald ein eindeutiger Ausgangswert in den Datenreihen gegeben ist, übernehmen wir diesen. Wenn dies nicht der Fall ist,
+            wenden wir die Methode von <a href="http://www.sdgindex.org/" target="_blank">SDSN</a> an:
+            Von den internationalen Länderdaten werden 2,5% der Anzahl der Werte gestrichen. Wenn es also 40 Beobachtungen gibt (Daten für 40 Länder), kann der niedrigste Werte gestrichen werden. Für Indikatoren ohne international vergleichbare Daten und ohne natürlichen Ausgangswert, orientieren wir uns an dem <a href="http://ec.europa.eu/eurostat/documents/3217494/8113874/KS-EZ-17-001-EN-N.pdf/c810af1c-0980-4a3b-bfdd-f6aa4d8a004e" target="_blank">Basisjahr 2008 der EU2020 Strategie</a>.
+          </p>
+        </section>
+
+        <section>
+          <h2>Wie werden Zielwerte berechnet?</h2>
+          <p>Bei der Verwendung der Deutschen Nachhaltigkeitsstrategie (DNS) Indikatoren übernehmen wir die offiziellen Zielwerte.</p>
+          <p>Für die ergänzenden Indikatoren wird der Zielwert von uns ausgewählt – beruhend auf internationalen Abkommen, Fachliteratur oder Expertengesprächen.</p>
+          <p>In den Fällen, in denen wir keinen Zielwert haben, nehmen wir den Durchschnitt der "5 Top Performer", sprich die fünf besten Werte.</p>
+        </section>
+
+        <section>
+          <h2>Woher kommen die aktuellen Werte?</h2>
+          <p>Wir nehmen die aktuellsten verfügbaren Werte. Das bedeutet für die Werte der Deutschen Nachhaltigkeitsstrategie (DNS), dass wir in einigen Fällen aktuellere Werte als die DNS haben, wenn zum Beispiel die OECD-Werte aktueller sind.</p>
+        </section>
+
+        <section>
+          <h2>Welche Länder werden zum Vergleich herangezogen?</h2>
+          <p>Wir nehmen entweder Daten von den EU28 oder OECD mit Präferenz für den Datensatz mit den aktuellsten Werten.</p>
+        </section>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {}
+    },
+    components: {},
+    head () {
+      return {
+        title: 'Methodik | 2030Watch',
+        meta: [
+          { hid: 'description', name: 'description', content: 'Methodik' }
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "~@/assets/style/variables";
+
+  .content {
+    section {
+      &:nth-child(even) {
+        background: #fff;
+      }
+    }
+  }
+
+  ul li, ol li {
+    list-style: inherit;
+  }
+
+  ol {
+    padding-left:1rem;
+    margin-left:0;
+  }
+
+</style>
