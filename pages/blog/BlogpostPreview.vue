b8ddc3da50245c921b994496e96ed81769e98edd
Jenny Gebske <jennifer@gebske.de>
2018-09-05 19:40:29 +0200

    adjust margins on blog post preview page

--- pages/blog/BlogpostPreview.vue
+++ pages/blog/BlogpostPreview.vue
@@ -4,9 +4,11 @@
     <div class="blog-post">
       <div class="post-teaser">
         <span class="caption">31. August 2018</span>
-        <nuxt-link to="/blog/2018/08/31/wir-muessen-reden">
-          <h3>Wir müssen reden – Wie ambitioniert ist unsere Nachhaltigkeitsdebatte wirklich?</h3>
-        </nuxt-link>
+        <div class="blog-post-title">
+          <nuxt-link to="/blog/2018/08/31/wir-muessen-reden">
+            <h3>Wir müssen reden – Wie ambitioniert ist unsere Nachhaltigkeitsdebatte wirklich?</h3>
+          </nuxt-link>
+        </div>
         <div>
           Ab sofort ist unsere Projektseite zur nationalen Umsetzung der globalen Nachhaltigkeitsziele in Deutschland mit allen 17 Zielen und ihren Indikatoren komplett. Auf 2030-watch.de könnt ihr mehr über die Fortschritte der offiziellen Ziele und ihre jeweiligen Indikatoren lernen. Zudem bieten wir zahlreiche ergänzende Indikatoren zur Erweiterung des Nachhaltigkeitsbildes in Deutschland. So könnt ihr einen Perspektivwechsel erleben und der Frage nachgehen: Wie ambitioniert ist die Nachhaltigkeitsdebatte in Deutschland? <nuxt-link to="/blog/2018/08/31/wir-muessen-reden"><span>Mehr lesen …</span></nuxt-link>
         </div>
@@ -26,9 +28,11 @@
       </div>
       <div class="post-teaser">
         <span class="caption">6. Juni 2018</span>
-        <nuxt-link to="/blog/2018/06/06/gleise-auf-nachhaltigkeit-umstellen">
-          <h3>Gleise auf Nachhaltigkeit umstellen – wie weit ist der Weg eigentlich noch?</h3>
-        </nuxt-link>
+        <div class="blog-post-title">
+          <nuxt-link to="/blog/2018/06/06/gleise-auf-nachhaltigkeit-umstellen">
+            <h3>Gleise auf Nachhaltigkeit umstellen – wie weit ist der Weg eigentlich noch?</h3>
+          </nuxt-link>
+        </div>
         <div>
           Das Thema Nachhaltigkeit wurde letzte Woche mehrmals in Deutschland unter die Lupe genommen. Ein internationales Experten*innen-Gremium stellte gestern einen <a href="https://www.nachhaltigkeitsrat.de/projekte/peer-review/" target="_blank">Bericht</a> (Peer Review) vor. Die Expert*innen kommen darin zu dem Schluss, dass die Deutsche Nachhaltigkeitspolitik bisher noch nicht genügend greift. Gleichzeitig engagierten sich so viele Menschen wie nie zuvor bei den deutschen Aktionstagen "Taten für morgen" zum Thema Nachhaltigkeit. Klar ist, dass wir auf dem Weg zu einem nachhaltigeren Deutschland sind, jedoch nicht ausreichend viele Gleise umgestellt sind. Es steht also zur Debatte, wie (ehrgeizig) wir diese Transformation angehen.
           <nuxt-link to="/blog/2018/06/06/gleise-auf-nachhaltigkeit-umstellen"><span>Mehr lesen …</span></nuxt-link>
@@ -83,5 +87,9 @@ export default {
     @media (max-width: 600px) {
       display: none
     }
+  }
+
+  .blog-post-title {
+    margin-bottom: 0.7rem;
   }
 </style>
