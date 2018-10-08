2b1413bc98d4e408269fcd8f13f04645ce05cf60
Jenny Gebske <jennifer@gebske.de>
2018-10-09 00:31:42 +0200

    remove unused code

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,10 @@
 module.exports = {
   router: {
-    scrollBehavior (to, from, savedPosition) {
+    scrollBehavior: function (to, from, savedPosition) {
       if (to.hash) {
         return {
           selector: to.hash
         }
-      }
-      if (savedPosition) {
-        return savedPosition
-      } else {
-        return {x: 0, y: 0}
       }
     }
   },
